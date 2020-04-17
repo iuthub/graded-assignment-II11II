4b4ed71ef6fbde8b262c7a2cf7a284a6e33e7b11
Islam <skyart450@gmail.com>
2020-04-17 01:07:17 -0400

    database migration and auth done

---
+++ database/migrations/2020_04_17_030314_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+           
+            $table->unsignedBigInteger('user_id');
+            $table->string('task');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+            ->references('user_id')
+            ->on('users')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
